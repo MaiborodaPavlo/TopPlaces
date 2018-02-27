fd0bf04feeefd44e6a32e6ae9c0efccb99078a2b
Pavel Mayboroda <Successbloger1@gmail.com>
2018-02-27 14:18:25 +0200

    Initial Commit

---
+++ TopPlaces/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TopPlaces
+//
+//  Created by Pavel on 27.02.2018.
+//  Copyright © 2018 Pavel Maiboroda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
